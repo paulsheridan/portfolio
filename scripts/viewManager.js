079ca8efea219aab4ea0185711ae9497dfaf8da2
Paul Sheridan <paul.sheridan@me.com>
2016-01-14 08:47:49 -0800

    added styling and changed colors around. wrapped every script in an IFFE statement and eliminated for loops, left forEach.

--- scripts/viewManager.js
+++ scripts/viewManager.js
@@ -1,25 +1,22 @@
-pageView = {};
+(function(module) {
+  pageView = {};
 
-pageView.mainNav = function() {
-  $('.nav-bar').on('click', '.tab', function(event) {
-    event.preventDefault();
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-  $('.nav-bar .tab:first').click();
-};
+  pageView.mainNav = function() {
+    $('.nav-bar').on('click', '.tab', function(event) {
+      event.preventDefault();
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+    $('.nav-bar .tab:first').click();
+  };
 
-pageView.toggleNav = function(){
-  $('.hamburger').on('click', function(){
-  $('nav ul').slideToggle();
-  })
-};
+  pageView.initPage = function(){
+    Project.all.forEach(function(a){
+      $('#portfolio').append(a.postProj());
+    });
+  };
 
-pageView.initPage = function(){
-  Project.all.forEach(function(a){
-    $('#portfolio').append(a.postProj());
-  });
-};
+  pageView.mainNav();
 
-pageView.mainNav();
-pageView.toggleNav();
+  module.pageView = pageView;
+})(window);

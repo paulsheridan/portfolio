dc9fe73bfe986c815ebf8ebac0be1b32d1388e96
Paul Sheridan <paul.sheridan@me.com>
2016-02-01 20:30:41 -0800

    removed SQL database, changed project list.

--- scripts/projectSource.js
+++ scripts/projectSource.js
@@ -7,57 +7,49 @@
 
   Project.all = [];
 
-  Project.createTable = function(callback) {
-    webDB.execute(
-      'CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, title VARCHAR(50), githubURL VARCHAR(50), projectURL VARCHAR(50), publishedDate DATETIME, description TEXT, textClass VARCHAR (50), imgURL VARCHAR(50));',
-      callback
-    );
-  };
-
   Project.prototype.postProj = function(){
     var source = $('#project-template').html();
     var template = Handlebars.compile(source);
     return template(this);
   };
 
-  Project.prototype.insertProj = function (callback) {
-    webDB.execute(
-      [
-        {
-          'sql': 'INSERT INTO projects (title, githubURL, projectURL, publishedDate, description, textClass, imgURL) VALUES (?,?,?,?,?,?,?);',
-          'data': [this.title, this.githubURL, this.projectURL, this.publishedDate, this.description, this.textClass, this.imgURL],
-        }
-      ]
-    );
-  };
-
-  Project.loadAll = function(rows) {
-    Project.all = rows.map(function(ele) {
-      return new Project(ele);
+  Project.loadAll = function(rawData) {
+    rawData.forEach(function(ele) {
+      Project.all.push(new Project(ele));
     });
   };
 
-  Project.fetchAll = function(next) {
-    webDB.execute('SELECT * FROM projects ORDER BY publishedDate DESC', function(rows) {
-      if (rows.length) {
-        Project.loadAll(rows);
-        next();
-      } else {
-        $.getJSON('/data/projects.json', function(rawData) {
-          rawData.forEach(function(item) {
-            var project = new Project(item);
-            project.insertProj();
-          });
-          webDB.execute('SELECT * FROM projects', function(rows) {
-            Project.loadAll(rows);
-            next();
-          });
-        });
-      }
-    });
+  Project.fetchAll = function() {
+    if (localStorage.rawData) {
+      $.ajax({
+        type: 'HEAD',
+        url: 'data/projects.json',
+        success: function(data, message, xhr){
+          var neweTag = xhr.getResponseHeader('ETag');
+          if(neweTag === localStorage.savedeTag) {
+            Project.loadAll(JSON.parse(localStorage.rawData));
+            console.log('no change');
+            pageView.initPage();
+          } else {
+            $.getJSON('data/projects.json', function(rawData){
+              Project.loadAll(rawData);
+              localStorage.rawData = JSON.stringify(rawData);
+              localStorage.savedeTag = neweTag;
+              pageView.initPage();
+            });
+          }
+        },
+      });
+    } else {
+      $.getJSON('data/projects.json', function(data, message, xhr){
+        Project.loadAll(data);
+        localStorage.rawData = JSON.stringify(data);
+        localStorage.savedeTag = xhr.getResponseHeader('ETag');
+        pageView.initPage();
+      });
+    }
   };
 
-  Project.createTable();
   Project.fetchAll(pageView.initPage);
 
   module.Project = Project;
